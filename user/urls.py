f822d4035441400819047ef597338ca685c9b5ee
busra <busraaceval@gmail.com>
2023-07-14 01:57:23 +0300

    ADD: login operations for user app

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include  
 from .views import UserView
+from rest_framework.authtoken.views import obtain_auth_token
 
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
@@ -7,5 +8,6 @@ router.register('', UserView)
 
 # urlpatterns = router.urls
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('login', obtain_auth_token),
 ]
