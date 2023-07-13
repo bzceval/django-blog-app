9b55d1d57af4de62dd0c338097404a08a8d21bec
busra <busraaceval@gmail.com>
2023-07-14 01:58:34 +0300

    ADD: logout function for user app

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,16 @@ from django.urls import path, include
 from .views import UserView
 from rest_framework.authtoken.views import obtain_auth_token
 
+# ----------------------------------------------------------------
+# Logout function:
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['GET', 'POST'])
+def logout(request):
+    request.user.auth_token.delete() # Token Sil.
+    return Response({"message": 'User Logout: Token Deleted'})
+# ----------------------------------------------------------------
+
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('', UserView)
@@ -10,4 +20,5 @@ router.register('', UserView)
 urlpatterns = [
     path('', include(router.urls)),
     path('login', obtain_auth_token),
+    path('logout', logout),
 ]
