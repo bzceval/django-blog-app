9182f9079f7940516dd1de8928baf19ea2bab1c6
busra <busraaceval@gmail.com>
2023-07-14 01:10:23 +0300

    ADD: password validation and encrypt

--- user/serializers.py
+++ user/serializers.py
@@ -6,9 +6,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = [
-             # "password",
+            # "password",
             "last_login",
             "date_joined",
             "groups",
             "user_permissions",
         ]
+
+    def validate(self, attrs):
+        from django.contrib.auth.password_validation import validate_password # doğrulama fonksiyonu
+        from django.contrib.auth.hashers import make_password # şifreleme fonksiyonu
+        password = attrs['password'] # Password al.
+        validate_password(password) # Validation'dan geçir.
+        attrs.update(
+            {
+                'password': make_password(password) # Password şifrele ve güncelle.
+            }
+        )
+        return super().validate(attrs) # Eğer yukarıdakiler çalışmazsa Orjinal methodu çalıştır.
